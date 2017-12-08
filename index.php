168b5a1c12028a77ec1a918edd829134544f4236
Leandro Meleti <leandromeleti97@hotmail.com>
2017-12-08 22:09:14 +0100

    add template and custom changes
    add bootstrap template https://startbootstrap.com/template-overviews/creative/
    include everything in the public_html folder structure
    set navigation to fix
    add slideshow script
    add start page
    split header, navigator and footer part to seperate php files for template purpose
    change body width in creative.min.css for all desktop and large devices

---
+++ index.php
@@ -0,0 +1,8 @@
+<script>
+    var pwd = prompt("Please enter Password", "");
+    if(pwd == null || pwd != "bma2017") {
+    	window.history.back();
+    } else {
+    	window.location="public_html/index.php";
+	}
+</script>
