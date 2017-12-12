18148b2d22a7896d88863ba3c89b7f430735312b
Leandro Meleti <leandromeleti97@hotmail.com>
2017-12-12 21:53:05 +0100

    extract custom changes from template css

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -29,4 +29,5 @@
 
         <!-- Custom styles for this template -->
         <link href="css/creative.min.css" rel="stylesheet"/>
+        <link href="css/custom.css" rel="stylesheet"/>
     </head>
