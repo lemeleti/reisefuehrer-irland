5e3e85ec98edd2f61b99be511a968d148b63f534
Leandro Meleti <leandromeleti97@hotmail.com>
2017-12-20 17:51:31 +0100

    add content
    add more buildings

--- resources/templates/oasen_block.php
+++ resources/templates/oasen_block.php
@@ -53,3 +53,89 @@
     <a class="custom-url" href="#">mehr...</a>
 </div>
 <!-- Platz 2 -->
+<h3>St. Stephen's Green</h3>
+<pre class="text-white">St Stephen's Green, Dublin 2, Irland
+<a class="custom-url" target="_blank" href="http://ststephensgreenpark.ie/">http://ststephensgreenpark.ie/</a></pre>
+<hr class="light my-4">
+<table>
+    <tr>
+        <td class="text-left">Ungef&auml;hre Dauer des Besuches:</td>
+        <td class="text-right">-</td>
+    </tr>
+    <tr>
+        <td class="text-left">&Ouml;ffnungszeiten:</td>
+        <td class="text-right"><pre class="text-white">Mo-Sa 07:30-Sonnenuntergang
+        So &amp; Feiertage 09:30-Sonnenuntergang</pre></td>
+    </tr>
+    <tr>
+        <td class="text-left">Beschreibung:</td>
+        <td class="text-right"><pre class="text-white">Park / Oase</pre></td>
+    </tr>
+    <tr>
+        <td class="text-left">Preis:</td>
+        <td class="text-right">-</td>
+    </tr>
+</table>
+<br />
+<hr class="light my-4">
+<div class="w3-content w3-display-container slides">
+    <img src="img/portfolio/fullsize/1.jpg">
+    <img src="img/portfolio/fullsize/2.jpg">
+    <img src="img/portfolio/fullsize/3.jpg">
+    <img src="img/portfolio/fullsize/4.jpg">
+    <img src="img/portfolio/fullsize/5.jpg">
+    <img src="img/portfolio/fullsize/6.jpg">
+
+    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(this.parentElement,-1)">&#10094;</button>
+    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(this.parentElement,1)">&#10095;</button>
+</div>
+<hr class="light my-4">
+<div class="content">
+    <p>Der Park im innersten der Stadt l&auml;dt zum kurzen Verweilen ein. Der Park mit einem grossen Teich und viel Platz um sich eine kurze Pause zu g&ouml;nnen hat eine lange Geschichte. Der Stephen's Green wurde vor 1815 noch als Weideland f&uuml;r Schafe und K&uuml;he genutzt. Ab 1815 wurde das umliegende Land f&uuml;r Bebauungen freigegeben und befindet sich nun in mitten des Stadtzentrums. Dazu mal war das Ben&uuml;tzen des Parks nur f&uuml;r die Anwohner der neuen Bauten gedacht. Doch 1880 wurde der Park neu gestaltet von der Familie Guinness und Baron Ardilaun machte den Park der &Ouml;ffentlichkeit zug&auml;nglich. Der Park wird von den Angestellten rund um den Park gerne als Pausenplatz genutzt. Im Sommer finden im Pavillons Konzerte statt.</p>
+</div>
+<div class="show-more text-white">
+    <a class="custom-url" href="#">mehr...</a>
+</div>
+<!-- Platz 3 -->
+<h3>Merrion Square Park</h3>
+<pre class="text-white">Merrion Square, Dublin, &Eacute;ire
+<a class="custom-url" target="_blank" href="http://merrionsquare.ie/">http://merrionsquare.ie/</a></pre>
+<hr class="light my-4">
+<table>
+    <tr>
+        <td class="text-left">Ungef&auml;hre Dauer des Besuches:</td>
+        <td class="text-right">-</td>
+    </tr>
+    <tr>
+        <td class="text-left">&Ouml;ffnungszeiten:</td>
+        <td class="text-right">-</td>
+    </tr>
+    <tr>
+        <td class="text-left">Beschreibung:</td>
+        <td class="text-right">-</td>
+    </tr>
+    <tr>
+        <td class="text-left">Preis:</td>
+        <td class="text-right">-</td>
+    </tr>
+</table>
+<br />
+<hr class="light my-4">
+<div class="w3-content w3-display-container slides">
+    <img src="img/portfolio/fullsize/1.jpg">
+    <img src="img/portfolio/fullsize/2.jpg">
+    <img src="img/portfolio/fullsize/3.jpg">
+    <img src="img/portfolio/fullsize/4.jpg">
+    <img src="img/portfolio/fullsize/5.jpg">
+    <img src="img/portfolio/fullsize/6.jpg">
+
+    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(this.parentElement,-1)">&#10094;</button>
+    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(this.parentElement,1)">&#10095;</button>
+</div>
+<hr class="light my-4">
+<div class="content">
+    <p>-</p>
+</div>
+<div class="show-more text-white">
+    <a class="custom-url" href="#">mehr...</a>
+</div>
