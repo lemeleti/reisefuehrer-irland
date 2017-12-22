150c29e6889edf2255a64f47ce7d545b50af85e5
Leandro Meleti <leandromeleti97@hotmail.com>
2017-12-22 23:52:48 +0100

    rename home link

--- resources/templates/nav.php
+++ resources/templates/nav.php
@@ -4,7 +4,7 @@
         <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink width-80"
             id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Reisef&uuml;hrer Irland</a>
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Reisef&uuml;hrer</a>
                 <button class="navbar-toggler navbar-toggler-right custom-toggler" type="button"
                     data-toggle="collapse" data-target="#navbarResponsive"
                     aria-controls="navbarResponsive" aria-expanded="false"
