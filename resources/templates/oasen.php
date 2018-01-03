c22fca664be8a72ceaf8eee534a0a03a31d94e71
Leandro Meleti <leandromeleti97@hotmail.com>
2018-01-03 14:24:58 +0100

    append oasen content
    add images and informations
    add a new place

---
+++ resources/templates/oasen.php
@@ -0,0 +1,166 @@
+<h3 class="mb-4">Oasen</h3>
+<!-- TODO: insert map -->
+<p style="color: red;">Hier kommt die Karte f&uuml;r Oasen hin</p>
+<hr class="my-4">
+<h3 class="mb-4">Beschreibung</h3>
+<div class="content">
+	<p class="mb-4">Dieser Stadtrundgang ist kein Rundgang im klassischen
+		Sinn, denn bei Oasen geht es darum die Zeit zu geniessen oder eine
+		Pause zumachen. Deshalb stelle ich dir hier einige M&ouml;glichkeiten vor,
+		sich in einem der vielen Dubliner P&auml;rke zu erholen.</p>
+</div>
+<div class="show-more">
+	<a class="custom-url text-center" href="#">mehr...</a>
+</div>
+<!-- Platz 1 -->
+<hr class="my-4 large">
+<h3>Phoenix Park</h3>
+<pre>Phoenix Park, Dublin 8, Irland
+<a class="custom-url" target="_blank" href="http://www.phoenixpark.ie/">www.phoenixpark.ie</a>
+</pre>
+<hr class="my-4">
+<h4>Ungef&auml;hre Dauer des Besuches</h4>
+<pre>2 h</pre>
+<h4>Beschreibung</h4>
+<pre>Park / Oase / Zoo</pre>
+<hr class="my-4">
+<pre>Geniesse die Erholungsoase in mitten der Stadt.</pre>
+<div class="w3-content w3-display-container slides my-4">
+	<!-- TODO: Bild folgt (Phoenix Park) -->
+	<img src="img/portfolio/thumbnails/1.jpg" />
+
+	<button class="w3-button w3-black w3-display-left"
+		onclick="plusDivs(this.parentElement,-1)">&#10094;</button>
+	<button class="w3-button w3-black w3-display-right"
+		onclick="plusDivs(this.parentElement,1)">&#10095;</button>
+</div>
+<div class="content">
+	<p>
+		Der Phoenix Park wird von der Dubliner Bev&ouml;lkerung als
+		Naherholungsgebiet gen&uuml;tzt. Mit seiner Gr&ouml;sse von 7.07 km<sup>2</sup>
+		ist er der gr&ouml;sste Park Europas. Er ist beinahe doppelt so gross wie
+		der Central Park in New York. Der Park ist nicht nach seinem
+		Namensvetter dem Feuervogel benannt, sondern der Name stammt aus dem
+		irischen "Fionn Uisce", was etwa so viel wie "klares Wasser" bedeutet.
+		Der Park wurde im 17. Jahrhundert f&uuml;r die englischen Gouverneure
+		errichtet und als Jagdgebiet genutzt. Seit 1747 steht der Park der
+		&Ouml;ffentlichkeit zur Verf&uuml;gung. Im Park finden das ganze Jahr hindurch
+		verschiedene Veranstaltungen statt, z.B. der 10 km "Phoenix Park Run"
+		oder die Kleingartenausstellung "Bloom". Wenn man sich ein wenig von
+		den Hauptwegen entfernt, kann man den Hirschherden beim Grasen
+		zusehen. Auch der Dubliner Zoo, der sich im Park befindet, bietet die
+		M&ouml;glichkeit, Tiere zu beobachten.
+	</p>
+</div>
+<div class="show-more">
+	<a class="custom-url text-center" href="#">mehr...</a>
+</div>
+<!-- Platz 2 -->
+<hr class="my-4 large">
+<h3>St. Stephen's Green</h3>
+<pre>St Stephen's Green, Dublin 2, Irland
+<a class="custom-url" target="_blank"
+		href="http://www.ststephensgreenpark.ie/">www.ststephensgreenpark.ie</a>
+</pre>
+<hr class="my-4">
+<h4>&Ouml;ffnungszeiten</h4>
+<pre>Mo-Sa 07:30-Sonnenuntergang
+So &amp; Feiertage 09:30-Sonnenuntergang</pre>
+<h4>Beschreibung</h4>
+<pre>Park / Oase</pre>
+<hr class="my-4">
+<div class="w3-content w3-display-container slides my-4">
+	<img src="img/oasen/fullsize/IMG_7426.JPG" />
+	<img src="img/oasen/fullsize/IMG_7431.JPG" />
+
+	<button class="w3-button w3-black w3-display-left"
+		onclick="plusDivs(this.parentElement,-1)">&#10094;</button>
+	<button class="w3-button w3-black w3-display-right"
+		onclick="plusDivs(this.parentElement,1)">&#10095;</button>
+</div>
+<div class="content">
+	<p>Der Park mit einem grossen Teich l&auml;dt zum Verweilen im Stadtinnern
+		ein. Dieser Park hat eine lange Geschichte. Der Stephen's Green wurde
+		vor 1815 noch als Weideland f&uuml;r Schafe und K&uuml;he genutzt. Ab 1815 wurde
+		das umliegende Land f&uuml;r Bebauungen freigegeben, so dass sich der Park
+		heute inmitten des Stadtzentrums befindet. Zuerst war die Benutzung
+		des Parks nur f&uuml;r die Anwohner der neuen Bauten gedacht. Doch 1880
+		wurde der Park neu gestaltet und von der Familie Guinness und Baron
+		Ardilaun der &Ouml;ffentlichkeit zug&auml;nglich gemacht. Manche Angestellten,
+		die rund um den Park arbeiten, g&ouml;nnen sich hier eine Pause. Im Sommer
+		finden im Pavillon Konzerte statt.</p>
+</div>
+<div class="show-more">
+	<a class="custom-url text-center" href="#">mehr...</a>
+</div>
+<!-- Platz 3 -->
+<hr class="my-4 large">
+<h3>Merrion Square Park</h3>
+<pre>Merrion Square, Dublin, &Eacute;ire
+<a class="custom-url" target="_blank" href="http://www.merrionsquare.ie/">www.merrionsquare.ie</a>
+</pre>
+<hr class="my-4">
+<h4>Schliessungszeiten</h4>
+<pre>Jan 16:30, Feb 17:00, M&auml;rz 18:00,
+April 20:00, Mai 21:00, Juni 21:30,
+Juli 21:30, Aug 21:00, Sept 20:00,
+Okt 19:00, Nov 17:00, Dez 16:30</pre>
+<hr class="my-4">
+<div class="w3-content w3-display-container slides my-4">
+	<img src="img/oasen/fullsize/IMG_7581.JPG" />
+
+	<button class="w3-button w3-black w3-display-left"
+		onclick="plusDivs(this.parentElement,-1)">&#10094;</button>
+	<button class="w3-button w3-black w3-display-right"
+		onclick="plusDivs(this.parentElement,1)">&#10095;</button>
+</div>
+<div class="content">
+	<p>Der im georgianischen Stil erbaute Merrion Square Park heisst
+		offiziell "Archbishop Ryan Park" und ist nach seinem Schenker benannt.
+		Denn dieser Platz wurde 1930 von der Kirche gepachtet zum Bau einer
+		Kathedrale. Jedoch kam es nie zur Verwirklichung dieser Pl&auml;ne. 1974
+		wurde der rechteckige Platz der Stadt Dublin geschenkt, die ihn als
+		Park er&ouml;ffnete. Der Park ist bekannt f&uuml;r seine Sammlung an
+		historischen Strassenlampen und Skulpturen. In der s&uuml;d&ouml;stlichen Ecke
+		des Parks befindet sich ein Luftschutzbunker aus der Zeit des Zweiten
+		Weltkriegs. In diesem Bunker konnten 1100 Personen Schutz vor
+		Luftangriffen finden. Seit 2010 ist der Park Teil des European Garden
+		Heritage Network.</p>
+</div>
+<div class="show-more">
+	<a class="custom-url text-center" href="#">mehr...</a>
+</div>
+<!-- Platz 4 -->
+<hr class="my-4 large">
+<h3>Iveagh Garden</h3>
+<pre>Clonmel St, Saint Kevin's, Dublin 2, Irland
+<a class="custom-url" target="_blank" href="http://www.iveaghgardens.ie/">www.iveaghgardens.ie</a>
+</pre>
+<hr class="my-4">
+<h4>&Ouml;ffnungszeiten</h4>
+<pre>Jan &amp; Dez Mo-Sa 08:00-15:30, So 10:00-15:30
+Feb &amp; Nov Mo-Sa 08:00-16:00, So 10:00-16:00
+M&auml;rz-Okt Mo-Sa 08:00-18:00. So 10:00-18:00
+&Ouml;ffnungszeiten k&ouml;nnen zwischen M&auml;rz und Oktober ver&auml;ndert sein da es Events gibt.</pre>
+<h4>Beschreibung</h4>
+<pre>Park / Oase</pre>
+<hr class="my-4">
+<div class="w3-content w3-display-container slides my-4">
+	<img src="img/oasen/fullsize/IMG_7619.JPG" />
+
+	<button class="w3-button w3-black w3-display-left"
+		onclick="plusDivs(this.parentElement,-1)">&#10094;</button>
+	<button class="w3-button w3-black w3-display-right"
+		onclick="plusDivs(this.parentElement,1)">&#10095;</button>
+</div>
+<div class="content">
+	<p>Der ehemalige Privatgarten der Guinness-Familie aus dem 19.
+		Jahrhundert ist ein &ouml;ffentlicher Garten, der ganz versteckt in der
+		Stadt liegt. Speziell an diesem Garten ist die einzige
+		Bogenschiessbahn von Irland, unter der ein Elefant begraben liegt. Das
+		Labyrinth, die Stauten, die Brunnen und der Wasserfall geben dem
+		Garten einen mystisch m&auml;rchenhaften Touch.</p>
+</div>
+<div class="show-more">
+	<a class="custom-url text-center" href="#">mehr...</a>
+</div>
