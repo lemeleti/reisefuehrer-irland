907ca94c2791c6c29780fed6120ead1e470460e7
Leandro Meleti <leandromeleti97@hotmail.com>
2018-01-16 22:12:45 +0100

    refactor meta tags

--- public_html/js/custom.js
+++ public_html/js/custom.js
@@ -39,7 +39,7 @@ $(".show-more a").on("click", function(e) {
 });
 
 //background fixing
-//TODO: check if works
+//TODO: check if works (does not work)
 if(is_client_apple_device()) {
 	$(window).scroll(function() {
 		  var scrolledY = $(window).scrollTop();
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <!-- TODO: complete meta tags -->
         <meta charset="UTF-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="author" content="co-authored by Anabel B., C&eacute;line G., Leandro Meleti, Silke B." />
-        <meta name="description" content="" />
+        <meta name="description" content="Reisetipps f&uuml;r Jugendliche mit wenig Budget in Dublin." />
         <meta name="keywords" content="reisef&uuml;hrer, dublin, tour, ferien, low-budget, irland, map, essen, restaurant, pub, museum, coffee"/>
 
         <title>Reisef&uuml;hrer Dublin</title>
