580a1a63d923c3cbdfbe3d372df87a8015582be6
Leandro Meleti <leandromeleti97@hotmail.com>
2018-01-30 23:52:28 +0100

    restyle content

--- public_html/quellen.php
+++ public_html/quellen.php
@@ -19,82 +19,99 @@ require_once TEMPLATES_PATH . '/header.php';
                 <div class="content-container">
                     <h2 class="section-heading text-left">Quellen</h2>
 <pre>
-1  www.stpatrickscathedral.ie Aufgerufen am 3.12.2017
+<b>1</b>  www.stpatrickscathedral.ie Aufgerufen am 3.12.2017
    https://www.dublin-inside.de/sehenswuerdigkeiten/st-pat.html Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 61)
-2  http://christchurchcathedral.ie/de/besucher/ Aufgerufen am 3.12.2017
+<b>2</b>  http://christchurchcathedral.ie/de/besucher/ Aufgerufen am 3.12.2017
    https://www.dublin-inside.de/sehenswuerdigkeiten/christ-church.html Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 60)
-3  http://www.dublincastle.ie/ Aufgerufen am 3.12.2017
+<b>3</b>  http://www.dublincastle.ie/ Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 56)
-4  https://www.stephensgreen.com/ Aufgerufen am 3.12.2017
+<b>4</b>  https://www.stephensgreen.com/ Aufgerufen am 3.12.2017
    https://de.wikipedia.org/wiki/St._Stephen%E2%80%99s_Green Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 40)
-5  http://www.nli.ie/ Aufgerufen am 3.12.2017
+<b>5</b>  http://www.nli.ie/ Aufgerufen am 3.12.2017
    https://de.wikipedia.org/wiki/Irische_Nationalbibliothek Aufgerufen am 3.12.2017
-6  www.tcd.ie Aufgerufen am 3.12.2017
+<b>6</b>  www.tcd.ie Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 20)
-7  https://www.dublin-inside.de/sehenswuerdigkeiten/half_penny_bridge.html Aufgerufen am 3.12.2017
+<b>7</b>  https://www.dublin-inside.de/sehenswuerdigkeiten/half_penny_bridge.html Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 25)
-8  https://de.wikipedia.org/wiki/Custom_House_(Dublin) Aufgerufen am 3.12.2017
+<b>8</b>  https://de.wikipedia.org/wiki/Custom_House_(Dublin) Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 27)
-9  https://de.wikipedia.org/wiki/Sean_O%E2%80%99Casey_Bridge Aufgerufen am 3.12.2017
+<b>9</b>  https://de.wikipedia.org/wiki/Sean_O%E2%80%99Casey_Bridge Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 26)
-10 https://www.dublin-inside.de/sehenswuerdigkeiten/samuel_beckett_bridge.html Aufgerufen am 3.12.2017
+<b>10</b> https://www.dublin-inside.de/sehenswuerdigkeiten/samuel_beckett_bridge.html Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 26)
-11 http://www.bordgaisenergytheatre.ie/ Aufgerufen am 3.12.2017
+<b>11</b> http://www.bordgaisenergytheatre.ie/ Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 12 / 82)
-12 BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 83)
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40 http://phoenixpark.ie/ Aufgerufen am 6.01.2018
+<b>12</b> BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 83)
+<b>13</b>
+<b>14</b>
+<b>15</b>
+<b>16</b>
+<b>17</b>
+<b>18</b>
+<b>19</b>
+<b>20</b>
+<b>21</b>
+<b>22</b>
+<b>23</b>
+<b>24</b>
+<b>25</b>
+<b>26</b>
+<b>27</b>
+<b>28</b>
+<b>29</b>
+<b>30</b>
+<b>31</b>
+<b>32</b>
+<b>33</b>
+<b>34</b>
+<b>35</b>
+<b>36</b>
+<b>37</b>
+<b>38</b>
+<b>39</b>
+<b>40</b> http://phoenixpark.ie/ Aufgerufen am 6.01.2018
    http://www.dublin.de/sehenswuerdigkeiten/freizeit-unterhaltung/phoenix-park/ Aufgerufen am 6.01.2018
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 67-70)
-41 http://ststephensgreenpark.ie/ Aufgerufen am 6.01.2018
+<b>41</b> http://ststephensgreenpark.ie/ Aufgerufen am 6.01.2018
    http://www.dublin.de/sehenswuerdigkeiten/freizeit-unterhaltung/saint-stephens-green-park/ Aufgerufen am 6.01.2018
-42 http://merrionsquare.ie/ Aufgerufen am 6.01.2018
+<b>42</b> http://merrionsquare.ie/ Aufgerufen am 6.01.2018
    https://de.wikipedia.org/wiki/Merrion_Square Aufgerufen am 6.01.2018
    DAVENPORT, FIONN (2016): DUBLIN. TOP SIGHTS / LOCAL LIFE / MADE EASY. (3.) LONELY PLANET. (S. 59)
-43 http://iveaghgardens.ie/ Aufgerufen am 6.01.2018
+<b>43</b> http://iveaghgardens.ie/ Aufgerufen am 6.01.2018
    DAVENPORT, FIONN (2016): DUBLIN. TOP SIGHTS / LOCAL LIFE / MADE EASY. (3.) LONELY PLANET. (S. 35)
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 84)
-44 http://www.dublin.de/sehenswuerdigkeiten/politik-regierung/four-courts/ Aufgerufen am 3.12.2017
+<b>44</b> http://www.dublin.de/sehenswuerdigkeiten/politik-regierung/four-courts/ Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 28)
-45 SOTHMANN, CHRISTINA (2007): DUBLIN. OSTFILDERN: DUMONT REISEVERLAG. (S. 22)
+<b>45</b> SOTHMANN, CHRISTINA (2007): DUBLIN. OSTFILDERN: DUMONT REISEVERLAG. (S. 22)
    http://www.dublincity.ie/ Aufgerufen am 3.12.2017
-46 BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 38)
+<b>46</b> BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 38)
    DAVENPORT, FIONN (2016): DUBLIN. TOP SIGHTS / LOCAL LIFE / MADE EASY. (3.) LONELY PLANET. (S. 39)
-47 http://www.oireachtas.ie/parliament/ Aufgerufen am 3.12.2017
+<b>47</b> http://www.oireachtas.ie/parliament/ Aufgerufen am 3.12.2017
    https://de.wikipedia.org/wiki/Leinster_House Aufgerufen am 3.12.2017
-48 https://www.taoiseach.gov.ie/eng/splash/ Aufgerufen am 3.12.2017
+<b>48</b> https://www.taoiseach.gov.ie/eng/splash/ Aufgerufen am 3.12.2017
    BIEGE, BERND (2017): DUBLIN. VOM ABRISSGEBIET ZUR PARTYMEILE. OSTFILDERN: DUMONT REISEVERLAG. (S. 82)
-49
-50
+<b>49</b>
+</pre>
+<h3 class="text-left">Bilder</h3>
+<pre>
+<b>National Library of Ireland:</b>
+https://commons.wikimedia.org/wiki/File:National_Library_of_Ireland_2011.JPG Aufgerufen am 22.01.2018
+Autor Yvonne M
+
+<b>Phoenixpark:</b>
+https://commons.wikimedia.org/wiki/File:Phoenix_Park_Dublin_01.JPG Aufgerufen am 22.01.2018
+Autor Superchilum
+
+<b>Four Courts Dublin (Politik):</b>
+https://en.wikipedia.org/wiki/File:Dublin_four_courts.JPG Aufgerufen am 22.01.2018
+
+<b>Leinster House:</b>
+https://commons.wikimedia.org/wiki/File:LeinsterHouseDublin2010.JPG Aufgerufen am 22.01.2018
+Autor Tebibyte
+
 </pre>
                 </div>
             </div>
