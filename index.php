dbbc93ae7ab93384b9f508356e6412d46458f85d
Leandro Meleti <leandromeleti97@hotmail.com>
2018-02-04 12:59:32 +0100

    remove password request

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <script>
-    var pwd = prompt("Please enter Password", "");
-    if(pwd == null || pwd != "bma2017") {
-        window.history.back();
-    } else {
+//     var pwd = prompt("Please enter Password", "");
+//     if(pwd == null || pwd != "bma2017") {
+//         window.history.back();
+//     } else {
         window.location="public_html/index.php";
-    }
+//     }
 </script>
