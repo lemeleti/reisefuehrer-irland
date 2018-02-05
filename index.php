3b4884f2dbd161237eeec49c8c179864e8f2fa9f
Leandro Meleti <leandromeleti97@hotmail.com>
2018-02-05 15:47:04 +0100

    remove unused file

--- index.php
+++
@@ -1,8 +0,0 @@
-<script>
-//     var pwd = prompt("Please enter Password", "");
-//     if(pwd == null || pwd != "bma2017") {
-//         window.history.back();
-//     } else {
-        window.location="public_html/index.php";
-//     }
-</script>
