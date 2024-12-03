2706754eab9960d47709f047858787f7c507e4e1
Leandro Meleti <Leandro.Meleti@netcetera.com>
2024-12-03 14:13:27 +0100

    fix google api key name

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -86,7 +86,7 @@ const GoogleMap: React.FC<GoogleMapProps> = ({
   );
 
   return (
-    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API}>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       {map}
     </APIProvider>
   );
